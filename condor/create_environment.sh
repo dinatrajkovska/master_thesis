ef52729ae49872e8f9b0be183f7b9310f4f1c0be
dinatrajkovska <dina.trajkovska@gmail.com>
2020-04-12 14:52:02 +0200

    testing condor alternative variant for running trainings

--- condor/create_environment.sh
+++ condor/create_environment.sh
@@ -1,7 +1,13 @@
-#!/usr/bin/env bash
+#! /usr/bin/env bash
 
 # Read .bashrc file from your home directory to load necessary packages
-source ~/.bashrc
+# source ~/.bashrc
 
 # Execute file specified as first argument to this shell file
-python $1
+# python $1
+
+
+
+# Dina's environment
+cd /users/students/r0691656/r0691656/exp/master_thesis;
+/users/students/r0691656/.cache/pypoetry/virtualenvs/audio-classification-thesis-Y9KjJEmP-py3.7/bin/python src/train_cross_val.py
